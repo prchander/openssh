9d14d98ae045590f0db0d29495a76ef043c14fe6
Pi <pchandra@uccs.edu>
2021-08-03 13:30:08 -0600

    dir changes

--- code/client.py
+++ code/client.py
@@ -1,7 +1,7 @@
 import os 
 import socket 
 
-opensshDir = os.path.expanduser('~/opehssh')
+opensshDir = os.path.expanduser('~/openssh')
 
 
 myCmd = f'{opensshDir}>./ssh -F {opensshDir}/regress/ssh_config -o KexAlgorithms=kyber-512-sha256 -o HostKeyAlgorithms=ssh-dilithium2>-o PubkeyAcceptedKeyTypes=ssh-dilithium2 -o PasswordAuthentication=no -i regress/ssh-dilithium2 somehost true'
